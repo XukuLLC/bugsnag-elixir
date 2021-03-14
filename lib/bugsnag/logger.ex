e7f287b12f3eb3b3995371138b5e255887782dc1
Dushyant Singh Shekhawat <contact@dushyantss.com>
2021-03-14 09:32:02 +0530

    Add liveview error handler
    * When the liveview crashes in mount, it'll be handled by the normal phoenix conn flow and does not need to be handled specifically.
    Ref: https://hexdocs.pm/phoenix_live_view/error-handling.html#exceptions-on-mount
    
    * When it crashes in events, handle it by the new pattern match in handle_event in logger.

--- lib/bugsnag/logger.ex
+++ lib/bugsnag/logger.ex
@@ -17,20 +17,56 @@ defmodule Bugsnag.Logger do
 
   def handle_event({:error_report, _gl, {_pid, _type, [message | _]}}, state)
       when is_list(message) do
+    case message[:initial_call] do
+      # do nothing in case of live_view
+      {Phoenix.LiveView.Channel, :init, _} ->
+        nil
+
+      _ ->
+        handle_error(message)
+    end
+
+    {:ok, state}
+  end
+
+  # Handle event for LiveView
+  def handle_event(
+        {:error, _gl,
+         {pid, _msg,
+          [
+            pid,
+            last_event,
+            %{socket: %{assigns: %{}} = socket},
+            {exception, stacktrace}
+          ]}},
+        state
+      )
+      when is_list(stacktrace) do
+    # Handle liveview
     try do
-      error_info = message[:error_info]
+      # We only want to send the last event for Phoenix.Socket.Message
+      last_event =
+        case last_event do
+          %{payload: payload} -> payload
+          _ -> nil
+        end
 
-      case error_info do
-        {_kind, {exception, stacktrace}, _stack} when is_list(stacktrace) ->
-          Bugsnag.report(exception, stacktrace: stacktrace)
+      base_metadata = %{last_event: last_event}
+      options = %{stacktrace: stacktrace}
 
-        {_kind, exception, stacktrace} ->
-          Bugsnag.report(exception, stacktrace: stacktrace)
-      end
+      options =
+        case extract_socket_metadata(socket) do
+          %{metadata: metadata} ->
+            Map.put(options, :metadata, Map.merge(base_metadata, metadata))
+
+          other when is_map(other) ->
+            options |> Map.put(:metadata, base_metadata) |> Map.merge(other)
+        end
+        |> Keyword.new()
+
+      Bugsnag.report(exception, options)
     rescue
-      ex ->
-        error_message = Exception.format(:error, ex)
-        Logger.warn("Unable to notify Bugsnag. #{error_message}")
+      ex -> report_failure(ex)
     end
 
     {:ok, state}
@@ -39,4 +75,49 @@ defmodule Bugsnag.Logger do
   def handle_event({_level, _gl, _event}, state) do
     {:ok, state}
   end
+
+  defp handle_error(message) do
+    try do
+      error_info = message[:error_info]
+
+      case error_info do
+        # Else do the following
+        {_kind, {exception, stacktrace}, _stack} when is_list(stacktrace) ->
+          Bugsnag.report(exception, stacktrace: stacktrace)
+
+        {_kind, exception, stacktrace} ->
+          Bugsnag.report(exception, stacktrace: stacktrace)
+      end
+    rescue
+      ex -> report_failure(ex)
+    end
+  end
+
+  defp report_failure(ex) do
+    error_message = Exception.format(:error, ex)
+    Logger.warn("Unable to notify Bugsnag. #{error_message}")
+  end
+
+  defp extract_socket_metadata(socket) do
+    extractor = Application.get_env(:bugsnag, :socket_metadata_extractor)
+
+    if extractor do
+      {module, function} = extractor
+      %{metadata: apply(module, function, [socket])}
+    else
+      extract_user_info(socket)
+    end
+  end
+
+  defp extract_user_info(socket) do
+    %{
+      user: socket.assigns[:current_user] |> user_info()
+    }
+  end
+
+  defp user_info(%{id: id, name: name, email: email}) do
+    %{id: id, email: email, name: name}
+  end
+
+  defp user_info(_), do: nil
 end
